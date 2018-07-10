f865ee134e59357d0b4f6126bd0e3d1b070be38f
Allythy <allythyrenan@gmail.com>
2018-07-10 09:47:59 -0300

    Definição de modelos para o app album

--- album/models.py
+++ album/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
+
+
+class Album(models.Model):
+    nome = models.CharField(max_length=255)
+    proprietario = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+class Foto(models.Model):
+    imagem = models.ImageField()
+    data = models.DateTimeField(auto_now_add=True)
+    album = models.ForeignKey(Album, on_delete=models.CASCADE)
+    aprovado = models.BooleanField(default=False)
