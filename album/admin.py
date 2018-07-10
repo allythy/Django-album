0420900ca2951be75cdd1d5864475c8b1a59c058
Allythy <allythyrenan@gmail.com>
2018-07-10 18:27:25 -0300

    Remoção do modelo Album

--- album/admin.py
+++ album/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 
 # Register your models here.
-from album.models import Album, Foto
+from album.models import Foto
 
-admin.site.register(Album)
 admin.site.register(Foto)
--- album/models.py
+++ album/models.py
@@ -4,13 +4,7 @@ from sorl.thumbnail import ImageField
 # Create your models here.
 
 
-class Album(models.Model):
-    nome = models.CharField(max_length=255)
-    proprietario = models.ForeignKey(User, on_delete=models.CASCADE)
-
-
 class Foto(models.Model):
     imagem = ImageField()
     data = models.DateTimeField(auto_now_add=True)
-    album = models.ForeignKey(Album, on_delete=models.CASCADE)
     aprovado = models.NullBooleanField(default=None, null=True)
